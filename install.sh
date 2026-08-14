03fc575c92d7459ddbac0de4bc11e2471af13cdd
XbibzOfficial777 <guebarbar12@gmail.com>
2026-08-14 11:17:54 +0000

    ui: replace startup banner with BibzCode artwork

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ umask 077
 
 VERSION="7.8.0-r6"
 RELEASE_ID="7.8.0-r6"
-EXPECTED_SHA256="a9037c2dbaea03156f87c018dbd102a1f907797be8efa50e7b83c23e2a4c050b"
+EXPECTED_SHA256="05fe38045bccf4e11bc064cbb660f4b4ee7958511f8ab1d39832e365da2a8892"
 CF_BASE="${DEEPSEEK_CF_BASE_URL:-https://deepseek-dash.bibzflow.workers.dev}"
 CF_ARCHIVE="$CF_BASE/releases/deepseek-cli-$RELEASE_ID.tar.gz"
 GH_ARCHIVE="${DEEPSEEK_GITHUB_RELEASE_URL:-https://raw.githubusercontent.com/XbibzOfficial777/deepseek-cli/main/releases/deepseek-cli-$RELEASE_ID.tar.gz}"
